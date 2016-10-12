e4657be94e673fdd2063c1eff5de5fa8a171d18b
cunjin <cunjin@gmail.com>
2016-10-12 22:04:35 +0700

    Fifth commit

--- basic.py
+++ basic.py
@@ -50,8 +50,28 @@ def insertDB_link(url):
     # disconnect from server
     db.close()
 
-if __name__== "__main__":
-    t = load_page("www.mataharimall.com","/","https")
+def load_link_from_db():
+    # Open database connection
+    db = MySQLdb.connect(host, user, passw, dba )
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    # Prepare SQL query to INSERT a record into the database.
+    sql = "SELECT * FROM t_crawllinks"
+    result = None
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Fetch all the rows in a list of lists.
+       results = cursor.fetchall()
+    except:
+       print "Error: unable to fecth data"
+
+    # disconnect from server
+    db.close()
+    return results
+
+def load_page_link(host, url, scheme):
+    t = load_page(host, url,scheme)
     #print(t)
     soup = BeautifulSoup(t, 'html.parser')
     t=soup.find_all('a')
@@ -64,3 +84,24 @@ if __name__== "__main__":
         else:
             url=(link.get('href'))
         insertDB_link(url)
+
+if __name__== "__main__":
+    links =  load_link_from_db()
+    i=0
+    total = len(links)
+    for url in links:
+        #print (url[1])
+        parsed_uri = urlparse(url[1])
+        scheme = parsed_uri.scheme
+        if(scheme==""):
+            scheme = "https"
+        if(parsed_uri.netloc==""):
+            host = "www.mataharimall.com"
+        else:
+            host = parsed_uri.netloc
+        url_link = parsed_uri.path
+        i+=1
+        print i, "of", total, scheme, host, url_link
+        load_page_link(host, url_link, scheme)
+        #domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        #print domain
