e108af9c6829ad52dbada2a0658dbbf90ae52fd0
cunjin <cunjin@gmail.com>
2016-10-12 21:46:12 +0700

    Fourth commit

--- basic.py
+++ basic.py
@@ -2,6 +2,14 @@
 
 import httplib
 from bs4 import BeautifulSoup
+import MySQLdb
+from urlparse import urlparse
+
+
+user = "root"
+passw = "lenovo"
+host = "localhost"
+dba = "crawldb"
 
 
 def load_page(host, url, scheme):
@@ -18,6 +26,30 @@ def load_page(host, url, scheme):
         print e
     return data
 
+def insertDB_link(url):
+    # Open database connection
+    db = MySQLdb.connect(host, user, passw, dba )
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to INSERT a record into the database.
+    sql = """INSERT INTO t_crawllinks (url, add_time)
+             VALUES ('%s', NOW())""" % (url,)
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Commit your changes in the database
+       db.commit()
+    except Exception, e:
+       print sql
+       print e
+       # Rollback in case there is any error
+       db.rollback()
+
+    # disconnect from server
+    db.close()
+
 if __name__== "__main__":
     t = load_page("www.mataharimall.com","/","https")
     #print(t)
@@ -31,3 +63,4 @@ if __name__== "__main__":
                 url= (link.get('href'))
         else:
             url=(link.get('href'))
+        insertDB_link(url)
